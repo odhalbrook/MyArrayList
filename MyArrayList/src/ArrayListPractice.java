1569b66b44c6ee99b4c6aee6b634cfc209de6ed7
owen.halbrook <owen.halbrook@MU-C1-12.mullen.pvt.k12.co.us>
2018-01-18 12:02:06 -0700

    commit added temps

--- MyArrayList/src/ArrayListPractice.java
+++ MyArrayList/src/ArrayListPractice.java
@@ -6,12 +6,35 @@ public class ArrayListPractice
 		public static void main(String[] args)
 			{
 				FillArray();
+				printTemps();
 
 			}
 
 		private static void FillArray()
 			{
-				// TODO Auto-generated method stub
+				temp.add(97.9);
+				temp.add(98.1);
+				temp.add(98.2);
+				temp.add(98.4);
+				temp.add(98.6);
+				temp.add(98.9);
+				temp.add(99.5);
+				temp.add(101.3);
+				temp.add(103.1);
+				temp.add(104.5);
+			}
+
+		private static void printTemps()
+			{
+				for(double s : temp)
+					{
+						System.out.println(s);
+					}
+				
+				for(int i = 0; i < temp.size() + 1; i++)
+					{
+						
+					}
 				
 			}
 
