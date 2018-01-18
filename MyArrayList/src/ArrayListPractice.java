fe4791cc2f6b9f2c9f601b7d84cb50f80a6beea7
owen.halbrook <owen.halbrook@MU-C1-12.mullen.pvt.k12.co.us>
2018-01-18 11:48:57 -0700

    Initial commit v2

---
+++ MyArrayList/src/ArrayListPractice.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class ArrayListPractice
+	{
+
+		static ArrayList <Double> temp = new ArrayList <Double>();
+		public static void main(String[] args)
+			{
+				FillArray();
+
+			}
+
+		private static void FillArray()
+			{
+				// TODO Auto-generated method stub
+				
+			}
+
+	}
